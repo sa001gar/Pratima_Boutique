56085fcab1a2605a8028ce63513e921f866e45dd
sa001gar <kundusagar233@gmail.com>
2025-10-25 23:41:43 +0530

    fix: ensure static and media files are served correctly in URL configuration

--- pratima_boutique/urls.py
+++ pratima_boutique/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+from django.conf.urls.static import static
 from core import views
 
 
@@ -29,6 +30,9 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('product/details/', views.product_individual, name='product_individual'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     # Include django_browser_reload URLs only in DEBUG mode
